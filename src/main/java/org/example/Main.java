b8204435a373d84bbfba161de378d137f058082e
huythai2004 <thaiphamhuy3@gmail.com>
2025-05-20 10:24:16 +0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,7 +18,7 @@ public class Main extends AbstractVerticle {
             response.setChunked(true);
 
             //Send data every second
-            long timeId = vertx.setPeriodic(1000, id -> {
+            long timeId = vertx.setPeriodic(3000, id -> {
                 String msg = "data: {\"testing\": true, \"sse_dev\": \"It's great\", \"now\": " + System.currentTimeMillis() + "}\n\n";
                 if(ctx.failed()){
                     System.out.println("Failed to send message!!");
